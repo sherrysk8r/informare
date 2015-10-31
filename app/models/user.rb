67276637e9a60ba320d9ba4e8c126e75f484966a
Sherry <sherryc1@andrew.cmu.edu>
2015-10-31 18:18:49 -0400

    Added some validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :liked_quotes
+
+	validates number_of_questions_answered, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank:true
+
+	validates number_of_questions_correct, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank:true
+
+	validates current_streak, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank:true
 end
