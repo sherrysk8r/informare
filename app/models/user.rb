a23d20f31ae7d063047c02a7c733fafe624cfc5e
Sherry <sherryc1@andrew.cmu.edu>
2015-12-05 17:00:06 -0500

    Removed current streak validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,7 @@ class User < ActiveRecord::Base
 	validates :number_of_questions_answered, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank:true
 
 	validates :number_of_questions_correct, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank:true
-
-	validates :current_streak, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_blank:true
-
+    
 	validates_presence_of :first_name, :last_name, :email
 	validates_uniqueness_of :email
 	validates_presence_of :password, on: :create 
